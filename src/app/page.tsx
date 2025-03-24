1c8b7f56d09e5fb963b929b1a5551c70a8785046
nohminist <nohminism@gmail.com>
2025-03-24 09:03:24 +0900

    test4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-// import Image from "next/image";
+// app/page.tsx
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>ゲーム攻略サイトテスト2</h1>
+        <h1>ゲーム攻略サイト</h1>
         <p>最新のゲーム攻略情報をお届けします！</p>
       </div>
     </main>
